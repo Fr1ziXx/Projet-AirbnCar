0be347ae9b0865953bdfb9fecd145917e44474b7
Fr1ziXx <lucas.pillot9@gmail.com>
2024-03-11 08:15:12 +0100

    début page reservation

--- src/views/ReservationView.vue
+++ src/views/ReservationView.vue
@@ -5,14 +5,13 @@ import { ref } from 'vue'
 export default {
   setup () {
     return {
-      model: ref('2020/07/08','2020/07/17')
+      model: ref({ from: '2020/07/08', to: '2020/07/17' })
     }
   },
   data() {
     return {
       car: null,
       store: car_store()
-
     }
   },
   created() {
@@ -30,20 +29,24 @@ export default {
 
 <template>
   <section class="row justify-center">
-    <q-card class="q-pa-sm q-pb-xl  container col-10 volkorn text-center">
-      <h2 class="text-white " style="text-decoration: underline; font-style: italic" >Réservation :</h2>
+    <q-card class="q-pa-sm q-pb-xl  container col-10 volkorn text-center text-white">
+      <h2 style="text-decoration: underline; font-style: italic" >Réservation :</h2>
       <h6 class="volkorn">Modèle : {{car.name}}</h6>
       <h6 class="volkorn">Prix par jour : {{car.prix}}</h6>
-      <h6 class="volkorn">Période de location :</h6>
-      <q-btn icon="event" round color="primary" class="q-mt-xl">
-        <q-popup-proxy @before-show="updateProxy" cover transition-show="scale" transition-hide="scale">
-          <q-date v-model="model" range />
-        </q-popup-proxy>
-      </q-btn>
-      <p>{{model}}</p>
-
+      <div class="row justify-center">
+        <h6 class="volkorn q-pr-md">Période de location :</h6>
+        <q-btn icon="event" round color="primary" class="q-mt-xl">
+          <q-popup-proxy @before-show="updateProxy" cover transition-show="scale" transition-hide="scale">
+            <q-date v-model="model" range />
+          </q-popup-proxy>
+        </q-btn>
+      </div>
+      <h6 class="q-mt-md q-mb-lg volkorn">Date de début : {{model.from}}</h6>
+      <h6 class="volkorn">Date de fin : {{model.to}}</h6>
       <h6 class="volkorn">Prix total : </h6>
-
+      <div class="row justify-center q-pt-xl">
+        <q-btn rounded push text-color="white" class="volkorn btn-reserv col-6" type="submit" label="Je réserve" style="font-size: 2.5em; background-color: #190d0d" @click="$router.push(`/`)"/>
+      </div>
 
 
     </q-card>
