a5f201069fe6c4737646f1b2cab67207e949cbf9
Victorfrancois <99280042+Victorfrancois@users.noreply.github.com>
2024-03-20 09:47:06 +0100

    changement reservation

--- src/views/ReservationView.vue
+++ src/views/ReservationView.vue
@@ -16,12 +16,13 @@
         <h6 class="volkorn">Date de début : {{ model.from }}</h6>
         <h6 class="volkorn">Date de fin : {{ model.to }}</h6>
         <h6 class="volkorn">Prix total : </h6>
-        <!-- Ajout des champs du formulaire avec v-model -->
+
+
         <input type="hidden" v-model="modele">
         <input type="hidden" v-model="Ddebut">
         <input type="hidden" v-model="Dfin">
         <div class="row justify-center q-pt-xl">
-          <!-- Correction: Ajout de l'attribut 'type="submit"' sur le bouton -->
+          
           <q-btn rounded push text-color="white" class="volkorn btn-reserv col-6" type="submit" label="Je réserve" style="font-size: 2.5em; background-color: #190d0d" />
         </div>
       </form>
@@ -55,19 +56,19 @@ export default {
     },
     enregistrer() {
       const formData = {
-        modele: this.car.name, // Utilisation de this.car.name pour le modèle
-        Ddebut: this.model.from, // Utilisation de this.model.from pour la date de début
-        Dfin: this.model.to // Utilisation de this.model.to pour la date de fin
+        modele: this.car.name, 
+        Ddebut: this.model.from, 
+        Dfin: this.model.to 
       };
 
       axios.post('reservation2.php', formData)
         .then(response => {
           console.log(response.data);
-          // Traiter la réponse si nécessaire
+          
         })
         .catch(error => {
           console.error(error);
-          // Traiter l'erreur si nécessaire
+        
         });
     }
   }
